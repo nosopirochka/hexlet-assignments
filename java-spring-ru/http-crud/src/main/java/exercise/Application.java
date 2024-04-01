2359ef85c6e1ca6914c4c72abd3ff5b54decfe94
@hexlet/cli <support@hexlet.io>
2024-04-01 15:47:30 +0300

    submit java-spring-ru/http-crud

---
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -0,0 +1,70 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/posts")
+    public List<Post> index(@RequestParam(defaultValue = "1") long page, @RequestParam(defaultValue = "10") long limit) {
+        return posts.stream()
+                .skip((page - 1) * limit)
+                .limit(limit)
+                .toList();
+    }
+
+    @GetMapping("/posts/{id}")
+    public Optional<Post> show(@PathVariable String id) {
+        return posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+    }
+
+    @PostMapping("/posts")
+    public Post create(@RequestBody Post post) {
+        posts.add(post);
+        return post;
+    }
+
+    @PutMapping("posts/{id}")
+    public Post update(@PathVariable String id, @RequestBody Post data) {
+        var somePost = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (somePost.isPresent()) {
+            var post = somePost.get();
+            post.setId(data.getId());
+            post.setTitle(data.getTitle());
+            post.setBody(data.getBody());
+        }
+        return data;
+    }
+
+    @DeleteMapping("posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+    // END
+}
